9f4dd57f53da1eabc64e409ceae272840c651cff
Iman Tabrizian <Tabrizian@outlook.com>
2017-07-20 15:03:57 +0430

    Nuxt initial configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,29 +1,31 @@
 module.exports = {
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'starter',
+    title: 'Bambil',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: 'Nuxt.js project'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700', rel: 'stylesheet', type: 'text/css'},
+      {href: 'https://fonts.googleapis.com/icon?family=Material+Icons', rel: 'stylesheet', type: 'text/css'}
     ]
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#3B8070' },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** Run ESLINT on save
-    */
+     ** Run ESLINT on save
+     */
     extend (config, ctx) {
       if (ctx.isClient) {
         config.module.rules.push({
@@ -34,5 +36,14 @@ module.exports = {
         })
       }
     }
-  }
+  },
+  css: [
+    'bootstrap-material-design/dist/css/bootstrap-material-design.css',
+    'bootstrap-material-design/dist/css/ripples.css',
+    {src: '~assets/App.scss', lang: 'scss'}
+  ],
+  modules: [
+    '@nuxtjs/bootstrap-vue'
+  ]
+
 }
