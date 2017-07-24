05179596d753ff1257d00543ac1eb0e8661c9d57
Iman Tabrizian <Tabrizian@outlook.com>
2017-07-24 14:06:49 +0430

    Adds carousel

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <section class="container">
-    <div>
-      <p align="center">
-        <img src="logo.png"/>
-      </p>
-    </div>
-  </section>
-</template>
+  <b-carousel height="300px">
 
+    <!-- Text slides -->
+    <b-carousel-slide height="300px" img="http://placeskull.com/600/300/ABABAB/-1/0">
+    </b-carousel-slide>
+
+    <!-- Slides with image -->
+    <b-carousel-slide height="300px" img="http://placeskull.com/600/300/f44336/-1/0">
+    </b-carousel-slide>
+
+  </b-carousel>
+
+
+</template>
