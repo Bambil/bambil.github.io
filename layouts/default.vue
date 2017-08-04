188947a5f39bb6c65af4a28f5b4474d81615e5e4
Parham Alvani <parham.alvani@gmail.com>
2017-08-04 17:40:13 +0430

    Adds menu button for courses.

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,6 +12,14 @@
             <v-list-tile-title>Home</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>class</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Courses</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar fixed>
