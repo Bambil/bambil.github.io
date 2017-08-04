ba808aa8e9f8d3f8682cbe454c6d190b7124a9dc
Parham Alvani <parham.alvani@gmail.com>
2017-08-05 01:18:39 +0430

    IoT course was added.

--- pages/courses.vue
+++ pages/courses.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-layout>
+  <v-layout row warp>
     <v-flex xs12 sm6 offset-sm3>
-      <v-card>
+      <v-card class="mb-2">
         <v-card-media
           class="white--text"
           height="200px"
@@ -23,8 +23,34 @@
           </div>
         </v-card-title>
         <v-card-actions>
-          <v-btn flat class="orange--text">Github</v-btn>
+          <v-btn flat class="orange--text" href="https://github.com/bambil/Python201">Github</v-btn>
           <v-btn flat class="orange--text" href="https://python.bambil.ml/">Slides</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card>
+        <v-card-media
+          class="white--text"
+          height="200px"
+          src="/img/iot.png"
+          >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">IoT 101</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-media>
+        <v-card-title>
+          <div>
+            <span class="grey--text">Parham Alvani &lt;parham.alvani@gmail.com&gt;</span><br>
+            <span>Introduction to IoT Platforms</span><br>
+            <span>SSC Advanced Python Summer Course - Summer 2017</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text" href="">Github</v-btn>
+          <v-btn flat class="orange--text" href="https://iot.bambil.ml/">Slides</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
