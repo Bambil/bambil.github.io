e4a134a64a453dba53f1f23169881fd387e85a3e
Parham Alvani <parham.alvani@gmail.com>
2017-08-05 00:52:31 +0430

    Adds python course into courses.

--- pages/courses.vue
+++ pages/courses.vue
@@ -1,2 +1,32 @@
 <template>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-card-media
+          class="white--text"
+          height="200px"
+          src="/static/doc-images/cards/docks.jpg"
+          >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Python 201</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-media>
+        <v-card-title>
+          <div>
+            <span class="grey--text">Parham Alvani &lt;parham.alvani@gmail.com&gt;</span><br>
+            <span>Introduction to Advanced Python</span><br>
+            <span>SSC Advanced Python Summer Course - Summer 2017</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text">Github</v-btn>
+          <v-btn flat class="orange--text" href="https://python.bambil.ml/">Slides</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
