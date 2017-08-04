d09af6ad81300a8043dde45eb86e914e64f85c47
Iman Tabrizian <Tabrizian@outlook.com>
2017-08-04 16:02:19 +0430

    Adds simple home button

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,15 +3,25 @@
     <v-navigation-drawer
       persistent
       v-model="drawer">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
     <v-toolbar fixed>
       <v-toolbar-side-icon @click.native.stop="drawer = !drawer">
       </v-toolbar-side-icon>
     </v-toolbar>
     <main>
-      <v-container fluid>
-        <nuxt />
-      </v-container>
+    <v-container fluid>
+      <nuxt />
+    </v-container>
     </main>
     <v-footer fixed>
       <span>&copy; Bambil 2017</span>
