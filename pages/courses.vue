7509d9bcf62daaf835778e143e4e9d9ef9bf0e0c
Iman Tabrizian <Tabrizian@outlook.com>
2017-08-05 14:18:33 +0430

    Adds node course

--- pages/courses.vue
+++ pages/courses.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout row warp>
-    <v-flex xs6 sm3 offset-sm3>
+    <v-flex xs6 sm3>
       <v-card>
         <v-card-media
           class="white--text"
@@ -53,6 +53,34 @@
         <v-card-actions>
           <v-btn flat class="orange--text" href="">Github</v-btn>
           <v-btn flat class="orange--text" href="https://iot.bambil.ml/">Slides</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+    <v-flex xs6 sm4>
+      <v-card>
+        <v-card-media
+          class="white--text"
+          height="200px"
+          src="/img/node.png"
+          >
+          <v-container fill-height fluid>
+            <v-layout fill-height>
+              <v-flex xs12 align-end flexbox>
+                <span class="headline">Node 101</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-media>
+        <v-card-title>
+          <div>
+            <span class="grey--text">Iman Tabrizian &lt;tabrizian@outlook.com&gt;</span><br>
+            <span>Node.JS + Hapi</span><br>
+            <span>SSC Node JS Summer Course - Summer 2017</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat class="orange--text" href="https://github.com/AUT-CEIT/node-course">Github</v-btn>
+          <v-btn flat class="orange--text" href="https://docs.google.com/presentation/d/15kh8KrlYm9YO5ySmY29qwrRBh0Kze7MM3aD1T5v5CMM/edit?usp=sharing">Slides</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
