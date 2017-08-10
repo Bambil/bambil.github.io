4e31df97726917405a6237af92779089121eaa02
Iman Tabrizian <Tabrizian@outlook.com>
2017-08-10 18:25:03 +0430

    Improves nuxt.config to support icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Bambil' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
   },
