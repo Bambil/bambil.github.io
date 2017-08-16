fdd4da95d29c4f5de143c30b88dfa1f54352d78d
Iman Tabrizian <Tabrizian@outlook.com>
2017-08-16 10:19:38 +0430

    Adds default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,96 +2,66 @@
   <v-app dark toolbar footer>
     <v-navigation-drawer
       persistent
-      :mini-variant="miniVariant"
+      temporary
       :clipped="clipped"
-      v-model="drawer"
-    >
+      v-model="drawer">
       <v-list>
-        <v-list-tile 
-          router
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-        >
+        <v-list-tile v-for='navItem in navItems'
+          :key='navItem.id'
+          :to='navItem.link'
+          :href='navItem.href'>
           <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
+            <v-icon>{{ navItem.icon }}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+            <v-list-tile-title>{{ navItem.title }}</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar fixed>
-      <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn 
-        icon
-        @click.native.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.native.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.native.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.native.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
+      <v-toolbar-side-icon @click.native.stop="drawer = !drawer">
+      </v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">Bambil</v-toolbar-title>
     </v-toolbar>
     <main>
-      <v-container fluid>
-        <nuxt />
-      </v-container>
+    <v-container fluid>
+      <nuxt />
+    </v-container>
     </main>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed">
-      <span>&copy; 2017</span>
+    <v-footer>
+      <span>&copy; Bambil 2017</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
-      }
+export default {
+  data() {
+    return {
+      drawer: false,
+      clipped: true,
+      navItems: [
+        {
+          id: 1,
+          title: 'Home',
+          icon: 'home',
+          link: '/'
+        },
+        {
+          id: 2,
+          title: 'Academy',
+          icon: 'account_balance',
+          link: '/academy'
+        },
+        {
+          id: 3,
+          title: 'Blog',
+          icon: 'format_bold',
+          href: 'https://blog.bambil.ml/'
+        }
+      ]
     }
   }
+}
 </script>
